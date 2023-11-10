452c47c3b573fefe99bc3d04a5735e58269734f0
niveK77pur <niveK77pur@users.noreply.github.com>
2023-11-10 23:37:56 +0000

    Apply automatic changes

--- .nvim.lua
+++
@@ -1,8 +0,0 @@
-require("nvls").setup({
-    lilypond = {
-        options = {
-            main_file = "Suki-ni-Natte-wa-Ikenai-Riyuu.ly",
-            include_dir = "./openlilylib",
-        },
-    },
-})
